cbf1a8f2ff46d734034ff72cde924638d104737f
Hariprasath <bala.hariprasath@gmail.com>
2019-04-12 22:48:49 +0530

    Lodash package usages

---
+++ nodeTest/Readme.md
@@ -0,0 +1,7 @@
+Install Dependeny:
+
+Lodash :
+    Ref: https://lodash.com/docs/
+    Desc: Simplify All string and array objects, extra functions
+
+`npm install lodash --save`
--- nodeTest/index.js
+++ nodeTest/index.js
@@ -1,6 +1,7 @@
 console.log("Started..!!!");
 const https = require("https");
 const fs = require("fs");
+const _ = require('lodash');
 
 // Custom Requires
 const nodeSt = require('./node-start');
@@ -13,8 +14,12 @@ const nodeSt = require('./node-start');
     });
 }); */
 
-console.log(nodeSt.addNote())
-var x = 5;
-var y = 6;
+// console.log(nodeSt.addNote())
 
-console.log(nodeSt.addVals(x, y));
+// console.log(nodeSt.addVals(5, -7));
+
+// console.log(_.isString("ABC"));
+// console.log(_.isString(23));
+
+var filteredArray = _.uniq(['Hari', 1,2,'Hari', 'hari', 1,3,2,24])
+console.log(filteredArray);
