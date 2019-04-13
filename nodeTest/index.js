2615e521f6d35c7348cbd886f7b9d5414c65a686
Hariprasath <bala.hariprasath@gmail.com>
2019-04-13 10:17:13 +0530

    7 getting input from user

--- nodeTest/index.js
+++ nodeTest/index.js
@@ -1,25 +1,24 @@
-console.log("Started..!!!");
+console.log("Hello Node");
 const https = require("https");
 const fs = require("fs");
 const _ = require('lodash');
-
-// Custom Requires
 const nodeSt = require('./node-start');
 
+// node index.js remove
+console.log(process.argv);
+var command = process.argv[2];
+console.log(command);
 
-/* https.get('https://raw.githubusercontent.com/hari4274/AngularForms/master/server/package.json', (res) => {
-    res.on('data', (d) => {
-        process.stdout.write(d);
-        fs.appendFile("response_json.json", d);
-    });
-}); */
+if (command == 'list') {
+    console.log("List all notes");
+} else if (command == 'read') {
+    console.log("Read Note");
+} else if (command == 'create') {
+    console.log("Note Created");
+} else if (command == 'remove') {
+    console.log("Note Removed");
+} else {
+    console.log("command Not Found");
+}
 
-// console.log(nodeSt.addNote())
 
-// console.log(nodeSt.addVals(5, -7));
-
-// console.log(_.isString("ABC"));
-// console.log(_.isString(23));
-
-var filteredArray = _.uniq(['Hari', 1,2,'Hari', 'hari', 1,3,2,24])
-console.log(filteredArray);
--- nodeTest/node-start.js
+++ nodeTest/node-start.js
@@ -1,11 +1,12 @@
-console.log("Node Started...");
-
+console.log("Node Started");
 module.exports.addNote = () => {
     console.log("Add note Called");
     return "New Note Created"
 }
 
-module.exports.addVals = (a, b) => {
-    console.log("Add function Called.");
-    return a + b;
-}
+/* https.get('https://raw.githubusercontent.com/hari4274/AngularForms/master/server/package.json', (res) => {
+    res.on('data', (d) => {
+        process.stdout.write(d);
+        fs.appendFile("response_json.json", d);
+    });
+}); */
